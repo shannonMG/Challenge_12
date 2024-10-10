1e3a5ae70fadacc03c37cbe5e16d6445af44aa1f
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-10-10 11:21:20 -0400

    added about me paragraph

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 
 const AboutMe = () => {
-    return <div>Will contain a picture, and a short bio of me</div>;
+    return <div>With a teaching background that shaped my love for simplifying the complex, my journey to becoming a developer feels like a natural evolution. I’ve learned that great teaching isn’t just about explaining concepts—it’s about making knowledge accessible and inspiring curiosity. That’s the spirit I bring to coding. Every line I write is crafted with the hope that it will be educational for someone else, whether they’re debugging, learning, or building on what I’ve started. I see development as an opportunity to not only create but also to teach through my code, ensuring that technology is approachable and empowering for everyone.</div>;
   };
 
 export default AboutMe;
