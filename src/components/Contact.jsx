5a1bb9403960d7ebaa38d3e366581d111f2c8b78
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-10-21 10:25:58 -0400

    afd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,30 +4,32 @@ import '../index.css'
 
 const Contact = () => {
   return (
-    <div className="card contact-card">
-      <h2 className= "contact-title">Let's Chat!</h2>
-      <form className="contact-form">
-        {/* Name Field */}
-        <div className="form-field">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
+    <div className="contact-container">
+      <div className="card contact-card">
+        <h2 className="contact-title">Let's Chat!</h2>
+        <form className="contact-form">
+          {/* Name Field */}
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
 
-        {/* Email Field */}
-        <div className="form-field">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
+          {/* Email Field */}
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
 
-        {/* Message Field */}
-        <div className="form-field">
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" rows="5" required></textarea>
-        </div>
+          {/* Message Field */}
+          <div className="form-field">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+          </div>
 
-        {/* Submit Button */}
-        <button type="submit" className="submit-button">Send Message</button>
-      </form>
+          {/* Submit Button */}
+          <button type="submit" className="submit-button">Send Message</button>
+        </form>
+      </div>
     </div>
   );
 };
